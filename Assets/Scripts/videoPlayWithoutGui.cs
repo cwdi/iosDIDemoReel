6a0a16f3be34f15a224d8d31a7d1569815f449a8
Curtis Walker <cwalker@dimin.com>
2015-09-15 17:58:38 -0500

    Change Scenes
    Added Splash screen and not so efficiently change to the next scene.

--- Assets/Scripts/videoPlayWithoutGui.cs
+++ Assets/Scripts/videoPlayWithoutGui.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class videoPlayWithoutGui : MonoBehaviour
 {
@@ -8,13 +9,18 @@ public class videoPlayWithoutGui : MonoBehaviour
 	//full movie -- sometimes slow connection  "http://download.blender.org/peach/bigbuckbunny_movies//big_buck_bunny_480p_h264.mov";
 	
 	public string videoForIndex1 = "https://devimages.apple.com.edgekey.net/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8";
-	
-	void Start ()
+	public string nextScene;
+	public float duration;
+
+	IEnumerator Start ()
 	{
 		//if you want  you can have add the player onto a game object  gameObject.AddComponent<VideoPlayer>();
 		video = GetComponent<VideoPlayer> ();// find a video player on this game object
 		if (video.videoIndex == 0) {
 			video.PlayTexture(videoForIndex0);
+			yield return new WaitForSeconds( duration );
+			video.Stop();
+			Application.LoadLevel(nextScene);
 		}
 		else
 			video.PlayTexture (videoForIndex1);
